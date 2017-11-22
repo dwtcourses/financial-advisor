fc3a5ee9510f5e32135add21dd3f15a22038f105
Kairi Kozuma <kairikozuma@gmail.com>
2017-11-21 20:08:29 -0500

    Rename for controllers

--- rest-api/routes/client_routes.js
+++ rest-api/routes/client_routes.js
@@ -1,16 +1,17 @@
 /* clients routing */
-module.exports = function(app) {
+exports = function(app) {
 
-    var clients = require('../controllers/client_controller')
+    // Import the client controller
+    var client_controller = require('../controllers/client_controller')
 
     // All clients
     app.route('/clients')
-        .get(clients.list_all_clients)
-        .post(clients.create_client)
+        .get(client_controller.list_all_clients)
+        .post(client_controller.create_client)
 
     // Single client
     app.route('/clients/:client_id')
-        .get(clients.read_client)
-        .put(clients.update_client)
-        .delete(clients.remove_client)
+        .get(client_controller.read_client)
+        .put(client_controller.update_client)
+        .delete(client_controller.remove_client)
 };
--- rest-api/routes/portfolio_routes.js
+++ rest-api/routes/portfolio_routes.js
@@ -1,16 +1,17 @@
 /* portfolios routing */
-module.exports = function(app) {
+exports = function(app) {
 
-        var portfolios = require('../controllers/portfolio_controller')
+    // Import the portfolio controller
+    var portfolio_controller = require('../controllers/portfolio_controller')
 
-        // All clients
-        app.route('/portfolios')
-            .get(portfolios.list_all_portfolios)
-            .post(portfolios.create_portfolio)
+    // All clients
+    app.route('/portfolios')
+        .get(portfolio_controller.list_all_portfolios)
+        .post(portfolio_controller.create_portfolio)
 
-        // Single client
-        app.route('/portfolios/:portfolio_id')
-            .get(portfolios.read_portfolio)
-            .put(portfolios.update_portfolio)
-            .delete(portfolios.remove_portfolio)
-    };
+    // Single client
+    app.route('/portfolios/:portfolio_id')
+        .get(portfolio_controller.read_portfolio)
+        .put(portfolio_controller.update_portfolio)
+        .delete(portfolio_controller.remove_portfolio)
+};
