14d5dd29e8cd375f9a25fa126b53f04175a5a126
Kairi Kozuma <kairikozuma@gmail.com>
2017-11-21 22:09:18 -0500

    Finish client api

--- rest-api/controllers/client_controller.js
+++ rest-api/controllers/client_controller.js
@@ -1,24 +1,67 @@
 /* Client controller */
 
-
+/** List all clients */
 exports.list_all_clients = async (req, res) => {
-    let query_result = await req.app.get('db').client.get_all()
-    return res.json(query_result.rows)
+    try {
+        let query_result = await req.app.get('db').client.get_all()
+        return res.json(query_result.rows)
+    } catch (err) {
+        console.log(err)
+        return res.status(500).json('Error retrieving all clients')
+    }
 }
 
-exports.create_client = function(req, res) {
-    return res.json('Create client')
+/** Create a single client and insert into database */
+exports.create_client = async (req, res) => {
+    try {
+        let query_result = await req.app.get('db').client.insert(
+            req.body.birthdate,
+            req.body.client_name,
+            req.body.remaining_funds,
+            req.body.country
+        )
+        return res.json('Created client')
+    } catch (err) {
+        console.log(err)
+        return res.status(500).json('Error creating client')
+    }
 }
 
+/** Retrieve a single client from the database */
 exports.read_client = async (req, res) => {
-    let query_result = await req.app.get('db').client.get(req.params.client_id)
-    return res.json(query_result.rows)
+    try {
+        let query_result = await req.app.get('db').client.get(req.params.client_id)
+        return res.json(query_result.rows)
+    } catch (err) {
+        console.log(err)
+        return res.status(500).json('Error retrieving client')
+    }
 }
 
-exports.update_client = function(req, res) {
-    return res.json('Update client')
+/** Update a single client */
+exports.update_client = async (req, res) => {
+    try {
+        await req.app.get('db').client.update(
+            req.params.client_id,
+            req.body.birthdate,
+            req.body.client_name,
+            req.body.remaining_funds,
+            req.body.country
+        )
+        return res.json('Successfully updated client')
+    } catch (err) {
+        console.log(err)
+        return res.status(500).json('Error updating client')
+    }
 }
 
-exports.remove_client = function(req, res) {
-    return res.json('Remove client')
+/** Remove a single client from the database */
+exports.remove_client = async (req, res) => {
+    try {
+        await req.app.get('db').client.delete(req.params.client_id)
+        return res.json('Successfully removed client')
+    } catch (err) {
+        console.log(err)
+        return res.status(500).json('Error removing client')
+    }
 }
--- rest-api/database/client.js
+++ rest-api/database/client.js
@@ -1,27 +1,64 @@
-
+/**
+ * Client Database Access Object
+ */
 module.exports = class Client {
 
+    /**
+     * Construct a client data access object
+     * @param {Pool} db_pool - Pool object to connect to PostgreSQL server
+     */
     constructor(db_pool) {
         this.db_pool = db_pool
     }
 
-    // Class methods
+    /** Get all clients
+     * @return promise of SQL query
+     */
     get_all() {
-        return this.db_pool.query('SELECT * FROM client;')
+        let command = 'SELECT * FROM client;'
+        return this.db_pool.query(command)
     }
 
-    insert() {
+    /** Insert a new client into the database
+     * @param {string} birthdate - date in MM/DD/YYYY format
+     * @param {string} client_name - full name of the client
+     * @param {float} remaining_funds - decimal number of dollars
+     * @param {string} country - name of country of client
+     * @return promise of SQL query
+     */
+    insert(birthdate, client_name, remaining_funds, country) {
+        let command = `INSERT INTO client (birthdate, client_name, remaining_funds, country) VALUES('${birthdate}', '${client_name}', ${remaining_funds}, '${country}');`
+        return this.db_pool.query(command)
     }
 
+    /** Get a client by client_id
+     * @param {integer} client_id - primary id of the client
+     * @return promise of SQL query
+     */
     get(client_id) {
-        return this.db_pool.query(`SELECT * FROM client WHERE client_id=${client_id};`)
+        let command = `SELECT * FROM client WHERE client_id=${client_id};`
+        return this.db_pool.query(command)
     }
 
-    update(client_id) {
-
+    /** Update single client details
+     * @param {integer} client_id - primary id of the client
+     * @param {string} birthdate - date in MM/DD/YYYY format
+     * @param {string} client_name - full name of the client
+     * @param {float} remaining_funds - decimal number of dollars
+     * @param {string} country - name of country of client
+     * @return promise of SQL query
+     */
+    update(client_id, birthdate, client_name, remaining_funds, country) {
+        let command = `UPDATE client SET birthdate='${birthdate}', client_name='${client_name}', remaining_funds=${remaining_funds}, country='${country}' WHERE client_id=${client_id}`
+        return this.db_pool.query(command)
     }
 
+    /** Remove a client by client_id
+     * @param {integer} client_id - primary id of the client
+     * @return promise of SQL query
+     */
     delete(client_id) {
-
+        let command = `DELETE FROM client WHERE client_id=${client_id}`
+        return this.db_pool.query(command)
     }
 }
