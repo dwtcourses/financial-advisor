f102c9c6be71b1674928a2fef0b5c71d90e3cf1e
Kairi Kozuma <kairikozuma@gmail.com>
2017-11-24 16:51:07 -0500

    Add new portfolio page

--- financial-advisor/routes/portfolios.js
+++ financial-advisor/routes/portfolios.js
@@ -19,6 +19,39 @@ router.get('/', async (req, res, next) => {
     }
 });
 
+/* GET portfolios add new page. */
+router.get('/add', async (req, res, next) => {
+    let options = {
+        uri: process.env.REST_HOST + '/clients',
+        json: true
+    }
+    try {
+        clients = await request.get(options);
+        return res.render('portfolio_add', {clients: clients})
+    } catch (err) {
+        console.log(err)
+        return res.status(500).json('Error getting clients')
+    }
+});
+
+/* POST create new portfolio */
+router.post('/add', async (req, res, next) => {
+    console.log(req.body)
+    let options = {
+        uri: process.env.REST_HOST + '/portfolios',
+        form: req.body,
+        json: true
+    }
+    try {
+        result = await request.post(options)
+        res.redirect('/portfolios/')
+    } catch (err) {
+        console.log(err)
+        return res.status(500).json('Error creating portfolio')
+    }
+});
+
+
 /* GET portfolio details */
 router.get('/:portfolio_id', async (req, res, next) => {
     let options = {
