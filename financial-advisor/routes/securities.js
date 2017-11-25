5340239f10e21d4da625a3dbbeb8defe87365e40
Kairi Kozuma <kairikozuma@gmail.com>
2017-11-25 09:30:07 -0500

    Add ticker and table to security details

--- financial-advisor/routes/securities.js
+++ financial-advisor/routes/securities.js
@@ -21,17 +21,30 @@ router.get('/', async (req, res, next) => {
 
 
 /* GET single security detail */
-router.get('/detail/:figi_id', async (req, res, next) => {
-    let options = {
+router.get('/details/:figi_id', async (req, res, next) => {
+    let detail_options = {
         uri: process.env.REST_HOST + '/securities/' + req.params.figi_id,
         json: true
     }
+    let price_options = {
+        uri: process.env.REST_HOST + '/securities/' + req.params.figi_id + '/prices',
+        json: true
+    }
     try {
-        result = await request.delete(options)
-        res.render('security_detail', { security: result });
+        detail_p = request.get(detail_options)
+        price_p = request.get(price_options)
+        Promise.all([detail_p, price_p]).then(values => {
+            details = values[0]
+            prices = values[1]
+            return res.render('security_detail',
+                {
+                    security: details,
+                    prices: prices
+                })
+        });
     } catch (err) {
         console.log(err)
-        return res.status(500).json('Error deleting recommendation ' + req.params.recommendation_id)
+        return res.status(500).json('Error retrieving security details for ' + req.params.figi_id)
     }
 });
 
