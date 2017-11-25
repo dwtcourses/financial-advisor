de6b9b55367775120ded17475c5fd12908cfeb36
Kairi Kozuma <kairikozuma@gmail.com>
2017-11-25 12:42:06 -0500

    Add Bollinger bands

--- financial-advisor/public/javascripts/security_chart.js
+++ financial-advisor/public/javascripts/security_chart.js
@@ -26,37 +26,65 @@ y.domain([d3.min(data, function (d) {
 })]);
 
 
-// define the line
-var valueline = d3.line()
-    .x(function (d) {
-        return x(d.date);
-    })
-    .y(function (d) {
-        return y(d.price);
-    });
+// Price line
+var line_price = d3.line()
+    .x(function (d) { return x(d.date); })
+    .y(function (d) { return y(d.price); });
+
+// Bollinger Bands
+var line_bbands_hi = d3.line()
+    .x(function (d) { return x(d.date); })
+    .y(function (d) { return y(d.bbands_hi); });
+
+var line_bbands_mid = d3.line()
+    .x(function (d) { return x(d.date); })
+    .y(function (d) { return y(d.bbands_mid); });
+
+var line_bbands_lo = d3.line()
+    .x(function (d) { return x(d.date); })
+    .y(function (d) { return y(d.bbands_lo); });
 
 // append the svg obgect to the body of the page
 // appends a 'group' element to 'svg'
 // moves the 'group' element to the top left margin
-var svg = d3.select("#chart")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform",
-        "translate(" + margin.left + "," + margin.top + ")");
+var svg = d3.select('#chart')
+    .append('svg')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+    .attr('transform',
+        'translate(' + margin.left + ',' + margin.top + ')');
 
-// Add the valueline path.
-svg.append("path")
+// Price path
+svg.append('path')
     .data([data])
-    .attr("class", "line")
-    .attr("d", valueline);
+    .attr('id', 'line-price')
+    .attr('class', 'line')
+    .attr('d', line_price);
 
-// Add the X Axis
-svg.append("g")
-    .attr("transform", "translate(0," + height + ")")
+// Bollinger Bands
+svg.append('path')
+    .data([data])
+    .attr('id', 'line-bbands-hi')
+    .attr('class', 'line')
+    .attr('d', line_bbands_hi);
+
+svg.append('path')
+    .data([data])
+    .attr('id', 'line-bbands-mid')
+    .attr('class', 'line')
+    .attr('d', line_bbands_mid);
+
+svg.append('path')
+    .data([data])
+    .attr('id', 'line-bbands-lo')
+    .attr('class', 'line')
+    .attr('d', line_bbands_lo);
+
+svg.append('g')
+    .attr('transform', 'translate(0,' + height + ')')
     .call(d3.axisBottom(x));
 
 // Add the Y Axis
-svg.append("g")
+svg.append('g')
     .call(d3.axisLeft(y));
