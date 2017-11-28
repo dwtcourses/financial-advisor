3126fe37e1be6aaef419b79882032fd2bdd05b91
Kairi Kozuma <kairikozuma@gmail.com>
2017-11-28 18:10:09 -0500

    Use ticker name for security details

--- rest-api/database/company.js
+++ rest-api/database/company.js
@@ -17,7 +17,7 @@ module.exports = class Company {
      */
     get_all(name) {
         if (name == undefined) {
-            let command = 'SELECT company.*, security.figi_id FROM company LEFT JOIN security ON security.company_id=company.company_id'
+            let command = 'SELECT company.*, security.figi_id, security.ticker FROM company LEFT JOIN security ON security.company_id=company.company_id'
             return this.db_pool.query(command)
         } else {
             let command = 'SELECT * FROM company WHERE company.company_name ~* $1'
@@ -31,7 +31,7 @@ module.exports = class Company {
      * @return promise of SQL query
      */
     get(company_id) {
-        let command = 'SELECT company.*, security.figi_id FROM company LEFT JOIN security ON security.company_id=company.company_id WHERE company.company_id=$1'
+        let command = 'SELECT company.*, security.figi_id, security.ticker FROM company LEFT JOIN security ON security.company_id=company.company_id WHERE company.company_id=$1'
         let params = [company_id]
         return this.db_pool.query(command, params)
     }
